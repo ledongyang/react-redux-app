f3424f0418e24854400ff50d32926024e707af0c
Michael Yang <ledongyang@gmail.com>
2018-03-12 20:58:05 -0400

    initial commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
+import { browserHistory } from 'react-router';
+
+class CoursePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
+  render () {
+    const {courses} = this.props;
+
+    return (
+      <div>
+        <h1>Courses page!</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}/>
+        <CourseList courses={courses}/>
+      </div>
+    );
+  }
+}
+
+CoursePage.propTypes = {
+  courses: PropTypes.array.isRequired
+  // createCourse: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    // createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursePage);
